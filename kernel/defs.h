c0e28e58af7ada2636a5c24e6d2b7e653158abe1
hyper <819740987@qq.com>
2025-11-07 14:54:34 +0800

    cow-not pass

--- kernel/defs.h
+++ kernel/defs.h
@@ -63,6 +63,9 @@ void            ramdiskrw(struct buf*);
 void*           kalloc(void);
 void            kfree(void *);
 void            kinit(void);
+void            kama_krefpage(void*);
+void*           kama_kcopy_n_deref(void* ) ;
+
 
 // log.c
 void            initlog(int, struct superblock*);
@@ -171,6 +174,8 @@ uint64          walkaddr(pagetable_t, uint64);
 int             copyout(pagetable_t, uint64, char *, uint64);
 int             copyin(pagetable_t, char *, uint64, uint64);
 int             copyinstr(pagetable_t, char *, uint64, uint64);
+int             kama_uvmcheckcowpage(uint64);
+int             kama_uvmcowcopy(uint64); 
 
 // plic.c
 void            plicinit(void);
